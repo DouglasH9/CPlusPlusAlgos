cf0ca48617b85b46e9c9e89e5f768b3a37eb883a
DouglasH9 <douglash9882@gmail.com>
2022-08-25 16:35:03 -0500

    HackerRank Algorithms

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@ vector<int> running_sum(const vector<int> &nums) {
         sum_vec.push_back(sum_vec.back() + nums[i]);
     return sum_vec;
 }
-
+//____________________________________________________________
 /*Find pivot index LeetCode. Given an array of integers nums, calculate the pivot index of this array.
 
 The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of
@@ -79,7 +79,7 @@ int pivot_index(const vector<int> &nums) {
     }
     return -1;
 }
-
+//___________________________________________________________________________
 /* Isomorphic strings Leetcode.
  * Given two strings s and t, determine if they are isomorphic.
 
@@ -107,7 +107,7 @@ bool is_isomorphic(const string &s, const string &t) {
     }
     return true;
 }
-
+//____________________________________________________________________________________________________
 /*Is subsequence algo Leetcode...
  *Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
@@ -130,14 +130,58 @@ bool is_subsequence(const string &s, const string &t) {
     }
     return match_count == s.length();
 }
-
+//___________________________________________________________________________
 /*Merge two sorted linked lists Leetcode
  * You are given the heads of two sorted linked lists list1 and list2.
 
 Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 
 Return the head of the merged linked list.*/
+//Node and list definition...
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val{0}, next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
+//recursive solution...
+ListNode* merge_two_lists_recursive(ListNode* list1, ListNode* list2) {
+    if(list1 == nullptr) {return list2;}
+    if(list2 == nullptr) {return list1;}
+
+    if (list1->val <= list2->val) {
+        list1 -> next = merge_two_lists_recursive(list1 -> next, list2);
+        return list1;
+    }
+    else {
+        list2 -> next = merge_two_lists_recursive(list1, list2 -> next);
+        return list2;
+    }
+}
+//merge two lists iterative...
+ListNode* merge_two_lists_iterative(ListNode* list1, ListNode* list2) {
+    auto* head = new ListNode(-1);
+    auto* tail = head;
+
+    while(list1 != nullptr && list2 != nullptr) {
+        //compare heads of both lists
+        if(list1->val <= list2->val) {
+            tail->next = list1;
+            list1 = list1->next;
+        }
+        else {
+            tail->next = list2;
+            list2 = list2->next;
+        }
+        tail = tail->next;
+    }
+    tail->next = list1 == nullptr ? list2 : list1;
+
+    head = head->next;
+    return head;
+}
 
 
 
