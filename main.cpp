602c32fd8761e37b91d8119a9cd49b668afbaa13
DouglasH9 <douglash9882@gmail.com>
2022-08-26 16:36:36 -0500

    HackerRank Algorithms

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <set>
 
 using namespace std;
 
@@ -218,7 +219,27 @@ ListNode* find_middle_node(ListNode* head) {
     return return_node;
 }
 
+//________________________________________________________________
+/*Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
 
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+Do not modify the linked list.*/
+
+ListNode* detect_cycle(ListNode* head) {
+    unordered_map<ListNode*, int> visited;
+
+    ListNode *node = head;
+
+    while(node != nullptr) {
+        if(visited[node] > 1) {
+            return node;
+        }
+        visited[node] += 1;
+        node = node->next;
+    }
+    return nullptr;
+}
 
 
 int main() {
