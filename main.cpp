b17054c37ef5878b9ed00361fb2cd6aac9f0526f
DouglasH9 <douglash9882@gmail.com>
2022-08-30 18:02:38 -0500

    HackerRank Algorithms

--- main.cpp
+++ main.cpp
@@ -355,6 +355,44 @@ int leet_bin_search(const vector<int> &nums, int target) {
     return -1;
 }
 
+/*_______________________________________________________________________
+ * First Bad Version Leetcode
+ * You are a product manager and currently leading a team to develop a new product.
+ * Unfortunately, the latest version of your product fails the quality check.
+ * Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one,
+ which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad.
+ Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+ Example:
+    Input: n = 5, bad = 4
+    Output: 4
+    Explanation:
+    call isBadVersion(3) -> false
+    call isBadVersion(5) -> true
+    call isBadVersion(4) -> true
+    Then 4 is the first bad version.*/
+bool is_bad_version(int bad_num) {
+    return bad_num == 10;
+}
+
+int first_bad_version(int n) {
+    int left{1};
+    int right{n};
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (is_bad_version(mid)) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
 
 int main() {
     vector<int> num_vec = { -1, 0, 3, 5, 9, 12 };
